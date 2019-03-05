0848b482c774d9bad854361b155a3ed2d0cf32df
PRAGMA <pragma.exe@gmail.com>
2019-03-05 02:01:47 +0000

    Added Assembly Info Data

--- --drm/Properties/AssemblyInfo.cs
+++ --drm/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("--drm")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("de-drm web downloading tool with multiple supported websites")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("PRAGMA")]
 [assembly: AssemblyProduct("--drm")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © PRAGMA 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
